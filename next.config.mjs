3387ef26b47b1bdec479e9c1083e3ef8c51fe5a1
mukeshkumargiri523 <mukeshkumargiri523@gmail.com>
2024-04-26 23:43:06 +0530

    updated files

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   //     appDir: true,
   //     serverComponentsExternalPackages: ["mongoose"],
   //   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
